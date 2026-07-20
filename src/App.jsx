4a4ed2be7e33814e18618d5d9766c8340868c594
saravananCodeExplorer <153452081+saravananCodeExplorer@users.noreply.github.com>
2026-07-20 21:44:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div>
+   <h1>Hello World!</h1>
+    
+      
+    </div>
+  )
+}
+
+export default App
