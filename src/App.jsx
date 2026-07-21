21d6869dd7e00bf6db847ef546c84422c99dad7c
saravananCodeExplorer <153452081+saravananCodeExplorer@users.noreply.github.com>
2026-07-21 09:06:00 +0530

    feat: add Intro component with styling and service cards; update App to include Intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
+import Intro from './components/Intro/Intro'
 
 const App = () => {
   return (
-    <div>
+    <>
 
-    <Hero/>
+      <Hero />
+      <Intro/>
       
-    </div>  
+    </>  
   )
 }
 
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,80 @@
+import "./Intro.css";
+
+const services = [
+  {
+    id: "01",
+    title: "NEW BUILDS",
+    image: "/src/assets/images/new_build.jpg",
+  },
+  {
+    id: "02",
+    title: "RENOVATIONS",
+    image: "/src/assets/images/renovations.jpg",
+  },
+  {
+    id: "03",
+    title: "DESIGN CONSULTS",
+    image: "/src/assets/images/design_consults.jpg",
+  },
+];
+
+const Intro = () => {
+  return (
+    <section className="intro">
+
+      <div className="intro-container">
+
+        {/* Logo */}
+        <div className="intro-logo">
+          <div className="logo-circle">
+            EJ
+          </div>
+        </div>
+
+        {/* Heading */}
+        <h2>
+          Cozy homes designed to
+          <br />
+          be lived in and loved.
+        </h2>
+
+        <p className="intro-text">
+          Taiyaki irony stumptown bespoke, normcore cold-pressed man bun
+          portland tilde blackbird spyplane organic VHS jean shorts.
+          Man braid bruh portland gentrify, try-hard jianbing pabst.
+        </p>
+
+        <a href="/" className="intro-btn">
+          SEE THE WORK
+        </a>
+
+        {/* Cards */}
+
+        <div className="service-grid">
+
+          {services.map((item) => (
+
+            <div className="service-card" key={item.id}>
+
+              <img
+                src={item.image}
+                alt={item.title}
+              />
+
+              <span>.{item.id}</span>
+
+              <h3>{item.title}</h3>
+
+            </div>
+
+          ))}
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+};
+
+export default Intro;
